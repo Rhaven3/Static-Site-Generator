a852c6efbf3ef7195577dbfa1bd71e36b423e89b
Rhaven <f.nom.utilisateur@gmail.com>
2025-10-15 22:28:23 +0200

    textNode to htmlNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,25 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+        inner_html = ''.join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{inner_html}</{self.tag}>"
